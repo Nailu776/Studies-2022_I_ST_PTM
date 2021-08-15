f6eedcfab45ab566ed4d979d256db31e74283e9a
Julian Helwig <julek.helwig@gmail.com>
2021-08-15 15:06:07 +0200

    Usunięcie kombinowania z fullscreenem.

--- Tetris/app/src/main/java/com/example/tetris/MainActivity.kt
+++ Tetris/app/src/main/java/com/example/tetris/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.tetris
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.view.WindowManager
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -26,9 +29,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //Fullscreen?
-        //this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
-
         setContentView(R.layout.activity_main)
 
         play_solo_button = findViewById(R.id.play_solo_button)
@@ -47,41 +47,39 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             exit_game.setOnClickListener(this)
 
         nick_name = findViewById(R.id.nick_name)
-
     }
-
     override fun onResume() {
         super.onResume()
         if (userNickname.isNotEmpty()) {
             nick_name.visibility = View.VISIBLE
-            nick_name.text = resources.getString(R.string.hi,"${userNickname}!")
+            nick_name.text = resources.getString(R.string.hi, "${userNickname}!")
         } else {
             nick_name.visibility = View.INVISIBLE
         }
     }
     override fun onClick(v: View?) {
         when(v){
-            play_solo_button ->{
-                val intent: Intent =
-                    Intent(this,TetrisGameActivity::class.java)
+            play_solo_button -> {
+                val intent =
+                        Intent(this, TetrisSoloGameActivity::class.java)
                 startActivity(intent)
             }
-            play_versus_button ->{
-                val intent: Intent =
-                    Intent(this,VersusGameActivity::class.java)
+            play_versus_button -> {
+                val intent =
+                        Intent(this, VersusGameActivity::class.java)
                 startActivity(intent)
             }
-            settings_button ->{
-                val intent: Intent =
-                    Intent(this,SettingsActivity::class.java)
+            settings_button -> {
+                val intent =
+                        Intent(this, SettingsActivity::class.java)
                 startActivity(intent)
             }
-            ranking_button ->{
-                val intent: Intent =
-                    Intent(this,RankingActivity::class.java)
+            ranking_button -> {
+                val intent =
+                        Intent(this, RankingActivity::class.java)
                 startActivity(intent)
             }
-            exit_game ->{
+            exit_game -> {
                 finish()
                 System.exit(0)
             }

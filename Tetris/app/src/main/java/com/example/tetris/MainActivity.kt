bb5e537c9f4064ee348d79e31b8959fe4c0d77ef
Julian Helwig <julek.helwig@gmail.com>
2021-08-18 18:24:42 +0200

    Naprawione aktywności: ranking, ustawienia, versus. Dodanie możliwości wyboru nicku.

--- Tetris/app/src/main/java/com/example/tetris/MainActivity.kt
+++ Tetris/app/src/main/java/com/example/tetris/MainActivity.kt
@@ -1,19 +1,25 @@
 package com.example.tetris
 
+
+import android.app.Activity
 import android.content.Intent
-import android.os.Build
+import android.net.Uri
+import android.opengl.Visibility
 import android.os.Bundle
 import android.view.View
-import android.view.WindowManager
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
+    // Ustawienia
     // Pseudonim gracza
     private var userNickname = ""
+    // Poziom startowy (działa tylko na soloq)
+    private var startLevel = 0
 
     // Te przyciski będą zainicjalizowane onCreate
     private lateinit var play_solo_button : Button
@@ -57,32 +63,51 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             nick_name.visibility = View.INVISIBLE
         }
     }
+
+    var settingsLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            // There are no request codes
+            val data: Intent? = result.data
+        }
+    }
+
+    val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+        // Handle the returned Uri
+    }
+
     override fun onClick(v: View?) {
         when(v){
             play_solo_button -> {
-                val intent =
-                        Intent(this, TetrisSoloGameActivity::class.java)
-                startActivity(intent)
+                startActivity(
+                    Intent(this, TetrisSoloGameActivity::class.java))
             }
             play_versus_button -> {
-                val intent =
-                        Intent(this, VersusGameActivity::class.java)
-                startActivity(intent)
+                startActivity(
+                    Intent(this, VersusGameActivity::class.java))
             }
             settings_button -> {
-                val intent =
+                startActivityForResult(
                         Intent(this, SettingsActivity::class.java)
-                startActivity(intent)
+                                .putExtra("startLevel", startLevel)
+                                .putExtra("userNickname", userNickname),76)
             }
             ranking_button -> {
-                val intent =
-                        Intent(this, RankingActivity::class.java)
-                startActivity(intent)
+                startActivity(Intent(this, RankingActivity::class.java))
             }
             exit_game -> {
                 finish()
                 System.exit(0)
             }
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when(requestCode){
+            76 -> {
+                userNickname = data?.getStringExtra("userNickname").toString()
+            }
+        }
+
     }
 }
